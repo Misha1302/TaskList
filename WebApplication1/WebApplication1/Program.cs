6336bbfd2cb5a643b202c61b88fe95091ac914c1
Михаил <misha13022008@gmail.com>
2023-09-07 20:46:32 +0300

    registration

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -37,10 +37,10 @@ app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapPost("/login", (Person loginData) =>
+app.MapPost("/login", (Person p) =>
 {
     // находим пользователя 
-    var person = people.FirstOrDefault(p => p.Email == loginData.Email && p.Password == loginData.Password);
+    var person = people.FirstOrDefault(x => x.Email == p.Email && x.Password == p.Password);
     // если пользователь не найден, отправляем статусный код 401
     if (person is null) return Results.Unauthorized();
 
@@ -60,6 +60,34 @@ app.MapPost("/login", (Person loginData) =>
     {
         access_token = encodedJwt,
         username = person.Email
+    };
+
+    return Results.Json(response);
+});
+app.MapPost("/authorize", (Person p) =>
+{
+    // находим пользователя 
+    var person = people.FirstOrDefault(x => p.Email == x.Email && p.Password == x.Password);
+    // если пользователь не найден, отправляем статусный код 401
+    if (person is not null) return Results.Problem();
+
+    var claims = new List<Claim> { new(ClaimTypes.Name, p.Email) };
+    // создаем JWT-токен
+    var jwt = new JwtSecurityToken(
+        AuthOptions.Issuer,
+        AuthOptions.Audience,
+        claims,
+        expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+        signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(),
+            SecurityAlgorithms.HmacSha256));
+    var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+    people.Add(p);
+
+    // формируем ответ
+    var response = new
+    {
+        access_token = encodedJwt,
+        username = p.Email
     };
 
     return Results.Json(response);
